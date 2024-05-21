fbb45a87805d0c9526a6c8f02a8660a186763c1c
wilsonma531 <wilsonma1992@gmail.com>
2024-05-21 10:23:25 +0100

    setting

--- frontend/src/Setting.jsx
+++ frontend/src/Setting.jsx
@@ -9,6 +9,12 @@ function Setting() {
       <div className="absolute top-[81px] left-[43px] [font-family:'Inter-Black',Helvetica] font-black text-black text-[40px] tracking-[0] leading-[normal] whitespace-nowrap">
         SETTING
       </div>
+      <div className="absolute top-[164px] left-[44px] [font-family:'Inter-Light',Helvetica] font-light text-black text-[24px] tracking-[0] leading-[normal]">
+        CHANGE PASSWORD
+      </div>
+      <div className="absolute top-[208px] left-[44px] [font-family:'Inter-Light',Helvetica] font-light text-black text-[24px] tracking-[0] leading-[normal]">
+        LOGOUT
+      </div>
       </div>
     </div>
   );
