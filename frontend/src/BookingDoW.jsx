04af69ae6c35cf0a6774945aa937d33fda94e43f
wilsonma531 <wilsonma1992@gmail.com>
2024-05-22 13:29:42 +0100

    day

--- frontend/src/BookingDoW.jsx
+++ frontend/src/BookingDoW.jsx
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
-import React from 'react';
-//import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function BookingDoW() {
+function UserBookingDate() {
+  const [bookingDate, setBookingDate] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:7001/api/reservation/20500')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setBookingDate(data.booking_day);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.toString());
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div>
-        MonDay
+      {bookingDate}
     </div>
   );
 }
 
-export default BookingDoW;
+export default UserBookingDate;
